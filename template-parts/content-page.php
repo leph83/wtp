22abfdc667d33e821353f86f858e8870dbd6e6f6
Phuc Le <info@phuc.at>
2020-02-28 20:31:44 +0100

    added empty paragraph

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,44 +9,32 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<?php the_content(); ?>
 
-	<?php wtp_post_thumbnail(); ?>
+<?php
+	wp_link_pages( array(
+		'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wtp' ),
+		'after'  => '</div>',
+	) );
+?>
 
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wtp' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'wtp' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
+<?php if ( get_edit_post_link() ) : ?>
+	<?php
+		edit_post_link(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'wtp' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
 				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+				get_the_title()
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+	?>
+<?php endif; ?>
