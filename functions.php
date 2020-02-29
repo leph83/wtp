d282f13808c06f98e8c1b4172418a7604f562f07
Phuc Le <info@phuc.at>
2020-02-29 19:11:36 +0100

    page transition only for non logged in user

--- functions.php
+++ functions.php
@@ -129,10 +129,10 @@ function wtp_scripts() {
 	wp_enqueue_script( 'wtp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '2020', true );
 	
 
-	// if (!is_user_logged_in()) {
+	if (!is_user_logged_in()) {
 		wp_enqueue_script( 'wtp-pagetransition', get_template_directory_uri() . '/js/swup.min.js', array(), '2020', true );
 		wp_enqueue_script( 'wtp-script', get_template_directory_uri() . '/js/script.js', array(), '2020', true );
-	// }
+	}
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
