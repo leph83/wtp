f00dc4db65d7c106de410e0a32d3fd6ee207d6c7
Phuc Le <info@phuc.at>
2020-03-01 19:54:38 +0100

    added scroll to top for page transition

--- js/script.js
+++ js/script.js
@@ -3,29 +3,10 @@ const options = {
     cache: false
 };
 
-// new SwupScrollPlugin({
-//     doScrollingRightAway: false,
-//     animateScroll: false,
-//     scrollFriction: 0,
-//     scrollAcceleration: 0,
-// });
-
-// const swup = new Swup(
-//     options,
-//     {
-//         plugins: [new SwupScrollPlugin()]
-//     }
-// );
-
-
 const swup = new Swup(
-    options,
-    { 
-        plugins: [new SwupScrollPlugin({ 
-            doScrollingRightAway: false, 
-            animateScroll: false, 
-            scrollFriction: 0, 
-            scrollAcceleration: 0, 
-        })] 
-    }
+    options
 );
+
+swup.on('contentReplaced', function () {
+    window.scrollTo(0, 0);
+});
