50be5b3fea2a3e54f92966781b0cb21ab3f133ce
Phuc Le <info@phuc.at>
2020-03-01 18:58:40 +0100

    removed page transition

--- js/script.js
+++ js/script.js
@@ -3,4 +3,29 @@ const options = {
     cache: false
 };
 
-const swup = new Swup(options);
+// new SwupScrollPlugin({
+//     doScrollingRightAway: false,
+//     animateScroll: false,
+//     scrollFriction: 0,
+//     scrollAcceleration: 0,
+// });
+
+// const swup = new Swup(
+//     options,
+//     {
+//         plugins: [new SwupScrollPlugin()]
+//     }
+// );
+
+
+const swup = new Swup(
+    options,
+    { 
+        plugins: [new SwupScrollPlugin({ 
+            doScrollingRightAway: false, 
+            animateScroll: false, 
+            scrollFriction: 0, 
+            scrollAcceleration: 0, 
+        })] 
+    }
+);
