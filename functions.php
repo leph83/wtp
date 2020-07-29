08282df12a2995f330c3d017ace37eb4b2283587
leph83 <info@phuc.at>
2020-07-29 10:27:16 +0200

    checkbox show title tagline working

--- functions.php
+++ functions.php
@@ -82,20 +82,28 @@ if ( !function_exists('wtp_load_styles') ) {
 /**
  * Blog Info
  */
-if ( !function_exists('wtp_blog_info') ) {
+ if ( !function_exists('wtp_blog_info') ) {
     function wtp_blog_info() {
-        $brand = '
-            <a href="'. esc_url(home_url('/')) .'" title="'. esc_html(get_bloginfo('name')) .'" rel="home">
-                '. esc_html(get_bloginfo('name')) .'
-            </a>
-            <span id="site-description">'. get_bloginfo('description').'</span>
-        ';
+        $brand = '';
 
         if ( has_custom_logo() ) {
             $custom_logo_id = get_theme_mod( 'custom_logo' );
-            $image = wp_get_attachment_image_src( $custom_logo_id , 'medium' );
+            $image = wp_get_attachment_image_src( $custom_logo_id, 'medium' );
 
-            $brand = '<img src="' . $image[0] . '" alt="'.esc_html(get_bloginfo('name')).'" width="'.$image[1].'" height="'.$image[2].'">';
+            $brand .= '
+                <a href="'. esc_url(home_url('/')) .'" title="'. esc_html(get_bloginfo('name')) .'" rel="home">
+                    <img src="' . $image[0] . '" alt="'.esc_html(get_bloginfo('name')).'" width="'.$image[1].'" height="'.$image[2].'">
+                </a>'
+            ;
+        }
+
+        if (get_theme_mod('header_text')) {
+            $brand .= '
+                <a href="'. esc_url(home_url('/')) .'" title="'. esc_html(get_bloginfo('name')) .'" rel="home">
+                    '. esc_html(get_bloginfo('name')) .'
+                </a>
+                <span id="site-description">'. get_bloginfo('description').'</span>
+            ';
         }
 
         return $brand;
