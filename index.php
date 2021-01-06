a8c020f722706023ed3cc5d7498ddbf9504accec
leph83 <info@phuc.at>
2021-01-06 11:19:57 +0100

    simplified featured image

--- index.php
+++ index.php
@@ -1,46 +1,44 @@
-<?php 
+<?php
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
 
-    if ( ! defined( 'ABSPATH' ) ) {
-        exit; // Exit if accessed directly.
-    }
+get_header();
 
-    get_header(); 
+$featured_image = '';
+if (is_home() && get_option('page_for_posts')) {
+    $id = get_post_thumbnail_id(get_option('page_for_posts'));
+    $featured_image = wp_get_attachment_image($id, 'full');
+}
 
-    $featured_image = '';
-    if (is_home() && get_option('page_for_posts') ) {
-        $img = wp_get_attachment_image_src(get_post_thumbnail_id(get_option('page_for_posts')),'full'); 
-
-        $featured_image = '';
-        if ( $img && $img[0] ) {
-            $featured_image = '<img src="' . $img[0] . '">';
-        }
-    }
 ?>
-    <section>
-        <div class="block  block--hero">
-            <div class="block__media">
-                <?php echo $featured_image; ?>
-            </div>
 
-            <div class="block__content">
-                <header class="block__header">
-                    <h1 class="block__title">
-                        <?php echo single_post_title(); ?>
-                    </h1>
-                </header>
-            </div>
+<section>
+    <div class="block  block--hero">
+        <div class="block__media">
+            <?php echo $featured_image; ?>
         </div>
 
-        <?php if (have_posts()) : ?>
-            <div class="grid  grid--auto">
-                <?php while (have_posts()) : the_post(); ?>
+        <div class="block__content">
+            <header class="block__header">
+                <h1 class="block__title">
+                    <?php echo single_post_title(); ?>
+                </h1>
+            </header>
+        </div>
+    </div>
 
-                    <?php get_template_part('template-parts/content', 'overview'); ?>
+    <?php if (have_posts()) : ?>
+        <div class="grid  grid--auto">
+            <?php while (have_posts()) : the_post(); ?>
 
-                <?php endwhile; ?>
+                <?php get_template_part('template-parts/content', 'overview'); ?>
 
-                <?php get_template_part('template-parts/nav', 'pagination'); ?>
-            </div>
-        <?php endif; ?>
-    </section>
+            <?php endwhile; ?>
+
+            <?php get_template_part('template-parts/nav', 'pagination'); ?>
+        </div>
+    <?php endif; ?>
+</section>
+
 <?php get_footer();
