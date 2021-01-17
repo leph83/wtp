65320f9d058cb1053c4b10992eabab9f32410bed
leph83 <info@phuc.at>
2021-01-17 20:37:44 +0100

    more cleaning

--- inc/enable/enable_css-defer.php
+++ inc/enable/enable_css-defer.php
@@ -4,23 +4,23 @@ if (!defined('ABSPATH')) {
 }
 
 
+
 /**
  * INCREASE PERFORMANCE
  * LOOP THROUGH CSS FILES AND ADD REL PRELOAD FOR EACH FILE
  */
-if (! function_exists('wtp_add_rel_preload')) {
-function wtp_add_rel_preload($html, $handle, $href, $media)
-{
-    if (is_admin())
-        return $html;
+if (!function_exists('wtp_add_rel_preload')) {
+    function wtp_add_rel_preload($html, $handle, $href, $media)
+    {
+        if (is_admin())
+            return $html;
 
-    $html .= <<<EOT
+        $html .= <<<EOT
     <link rel='preload' as='style' href='$href'>
     
     EOT;
 
-    return $html;
-}
-
-add_filter('style_loader_tag', 'wtp_add_rel_preload', 10, 4);
+        return $html;
+    }
+    add_filter('style_loader_tag', 'wtp_add_rel_preload', 10, 4);
 }
--- inc/enable/enable_gutenberg-theme.php
+++ inc/enable/enable_gutenberg-theme.php
@@ -3,4 +3,6 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
+
+
 add_theme_support('wp-block-styles');
