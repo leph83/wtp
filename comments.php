9621ed25ab11938b5c1b7f57f95f528c055e192b
leph83 <info@phuc.at>
2021-01-22 19:10:50 +0100

    added fallback for clamp

--- comments.php
+++ comments.php
@@ -6,7 +6,9 @@ if (!defined('ABSPATH')) {
 
 
 <?php if (have_comments()) : ?>
-    <div id="comments">
+
+    <div class="comments">
+
         <?php
         global $comments_by_type;
         $comments_by_type = separate_comments($comments);
@@ -14,37 +16,43 @@ if (!defined('ABSPATH')) {
 
         <?php if (!empty($comments_by_type['comment'])) : ?>
 
-            <section id="comments-list" class="comments">
-                <h3 class="comments-title"><?php comments_number(); ?></h3>
+            <section class="comment">
+                <h3 class="comment__title">
+                    <?php comments_number(); ?>
+                </h3>
 
                 <?php if (get_comment_pages_count() > 1) : ?>
-                    <nav id="comments-nav-above" class="comments-navigation">
-                        <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
+                    <nav class="comment__navigation">
+                        <div class="comment__links"><?php paginate_comments_links(); ?></div>
                     </nav>
                 <?php endif; ?>
 
-                <ul>
+                <ul class="comment__list">
                     <?php wp_list_comments('type=comment'); ?>
                 </ul>
 
 
                 <?php if (get_comment_pages_count() > 1) : ?>
-                    <nav id="comments-nav-below" class="comments-navigation">
-                        <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
+                    <nav class="comment__navigation">
+                        <div class="comment__links"><?php paginate_comments_links(); ?></div>
                     </nav>
                 <?php endif; ?>
             </section>
 
         <?php endif; ?>
 
+
+
         <?php if (!empty($comments_by_type['pings'])) : ?>
 
             <?php $ping_count = count($comments_by_type['pings']); ?>
 
-            <section id="trackbacks-list" class="comments">
-                <h3 class="comments-title"><?php echo '<span class="ping-count">' . esc_html($ping_count) . '</span> ' . esc_html(_nx('Trackback or Pingback', 'Trackbacks and Pingbacks', $ping_count, 'comments count', 'wtp')); ?></h3>
+            <section class="comment">
+                <h3 class="comment__title">
+                    <?php echo '<span class="comment__ping-count">' . esc_html($ping_count) . '</span> ' . esc_html(_nx('Trackback or Pingback', 'Trackbacks and Pingbacks', $ping_count, 'comments count', 'wtp')); ?>
+                </h3>
 
-                <ul>
+                <ul class="comment__list">
                     <?php wp_list_comments('type=pings&callback=wtp_custom_pings'); ?>
                 </ul>
             </section>
@@ -55,6 +63,12 @@ if (!defined('ABSPATH')) {
 
 <?php endif; ?>
 
+
+
+
+
 <?php if (comments_open()) : ?>
+
     <?php comment_form(); ?>
+
 <?php endif; ?>
