b84e5449a81d3a6f5a1eb4bcfc453942628c6b38
leph83 <info@phuc.at>
2021-01-22 11:51:46 +0100

    reduced more files, don't make decision for others in css reset

--- index.php
+++ index.php
@@ -5,78 +5,21 @@ if (!defined('ABSPATH')) {
 
 get_header();
 
-// DEFAULT
-$title = single_post_title('', false);
-$description = '';
-$featured_image = get_the_post_thumbnail();
-
-// BLOG PAGE
-if (is_home()) {
-    $featured_image = '';
-
-    // Blog not as Startpage
-    if (get_option('page_for_posts') != 0) {
-        $id = get_post_thumbnail_id(get_option('page_for_posts'));
-        $featured_image = wp_get_attachment_image($id, 'full');
-    }
-}
-
-// ARCHIVE
-if (is_archive()) {
-    $title = get_the_archive_title();
-    $description = get_the_archive_description();
-}
-
-// SEARCH
-if (is_search()) {
-    $title = _n(
-        'We found ' . (int) $wp_query->found_posts . ' result for',
-        'We found ' . (int) $wp_query->found_posts . ' results for',
-        'wtp'
-    ) . ' <span>"' . esc_html(get_search_query()) . '"</span>';
-
-    $description = get_search_form(false);
-}
-
-// 404
-if (is_404()) {
-    $title = esc_html('404', 'wtp');
-}
-
-
 ?>
-
-
-<div class="page__header  block  block--hero">
-    <div class="block__media">
-        <?php echo $featured_image; ?>
-    </div>
-
-    <div class="block__content  alignwide">
-        <header class="block__header">
-            <h1 class="page__title  block__title">
-                <?php echo $title; ?>
-            </h1>
-        </header>
-
-        <?php echo $description; ?>
-    </div>
-</div>
-
 
 <?php if (have_posts()) : ?>
 
     <?php while (have_posts()) : the_post(); ?>
 
-        <?php get_template_part('template-parts/content/content', 'overview'); ?>
+        <?php get_template_part('template-parts/content/content', 'content'); ?>
 
     <?php endwhile; ?>
 
-    <?php 
-        $args = array(
-            'class' => 'alignwide',
-        );
-        the_posts_navigation($args); 
+    <?php
+    $args = array(
+        'class' => 'alignwide',
+    );
+    the_posts_navigation($args);
     ?>
 
 <?php else : ?>
--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -3,10 +3,8 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-$title = get_the_title('', false);
-$image = get_the_post_thumbnail() ?? false;
-
-
+$title = '<a href="' . get_the_permalink() . '">' . get_the_title() . '</a>';
+$image = get_the_post_thumbnail(get_the_id(), 'large') ?? false;
 $author = esc_html('by ') . get_the_author_posts_link();
 $date = get_the_time(get_option('date_format'));
 
@@ -19,10 +17,15 @@ if ($posttags) {
     }
 }
 
+// SINGULAR
+if (is_singular()) {
+    $title = get_the_title();
+}
+
 // CATEGORIES
 $categories = get_the_category_list(', ');
 
-// POST INFO
+// POSTS
 $postmeta = '';
 if (get_post_type() == 'post') {
     $postmeta .=
@@ -32,50 +35,73 @@ if (get_post_type() == 'post') {
         .
         '<div class="">' . $tags . '</div>'
         .
-        '<div class="">' . $categories . '</div>';
+        '<div class="">' . $categories . '</div>
+    ';
+}
+
+
+
+// BLOG PAGE
+if (is_home()) {
+    $featured_image = '';
+
+    // Blog not as Startpage
+    if (get_option('page_for_posts') != 0) {
+        $id = get_post_thumbnail_id(get_option('page_for_posts'));
+        $featured_image = wp_get_attachment_image($id, 'full');
+    }
+}
+
+// ARCHIVE
+if (is_archive()) {
+    $title = get_the_archive_title();
+    $description = get_the_archive_description();
+}
+
+// SEARCH
+if (is_search()) {
+    $title = _n(
+        'We found ' . (int) $wp_query->found_posts . ' result for',
+        'We found ' . (int) $wp_query->found_posts . ' results for',
+        'wtp'
+    ) . ' <span>"' . esc_html(get_search_query()) . '"</span>';
+
+    $description = get_search_form(false);
+}
+
+// 404
+if (is_404()) {
+    $title = esc_html('404', 'wtp');
 }
 
 ?>
 
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('block  alignfull'); ?>>
-    <div class="block__media">
-        <?php echo $image; ?>
-    </div>
+<article id="post-<?php the_ID(); ?>" <?php post_class('block  margin padding'); ?>>
 
-    <div class="block__content">
-        <header class="block__heading">
-            <h1 class="block__title">
-                <?php echo $title; ?>
-            </h1>
+    <?php echo $image; ?>
 
-            <div>
-                <?php echo $postmeta; ?>
-            </div>
+    <h1>
+        <?php echo $title; ?>
+    </h1>
 
-        </header>
-    </div>
-</article>
+    <?php echo $postmeta; ?>
 
+    <?php if (is_singular()) : ?>
+        <?php the_content(); ?>
+    <?php else : ?>
+        <?php the_excerpt(); ?>
+    <?php endif; ?>
 
-<div class="entry-content">
-    <?php the_content(); ?>
-</div>
+    <?php
+    $args = array(
+        'before'    => '<nav class="alignwide">',
+        'after'     => '</nav>'
+    );
 
+    wp_link_pages($args);
+    ?>
 
-
-<?php
-$args = array(
-    'before'    => '<nav class="alignwide">',
-    'after'     => '</nav>'
-);
-
-wp_link_pages($args);
-?>
-
-
-
-
-<div class="alignwide">
     <?php comments_template('', true); ?>
-</div>
+
+</article>
